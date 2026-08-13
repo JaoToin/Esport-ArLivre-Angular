a579fd772955c99992ef2a40fb57d89aedfa3258
Joao <joao.a.guedes@aluno.senai.br>
2026-08-13 18:41:28 -0300

    style.css geral finalizado com cores necessarias; Feito a ligação do home e do atleta-componet no routes.ts; app.html removido o componente não ussado; menu, feito a importação do routerlink e finalizado o menu.html, iniciado o home.html e o home.css; no atleta-component, finalizado o html e o css por hora.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './component/menu-component/home/home';
+import { AtletaComponent } from './component/menu-component/atleta-component/atleta-component';
+
+export const routes: Routes = [
+  {
+    path:'',
+    redirectTo:"/home",
+    pathMatch: 'full'
+  },
+  {
+    path: "home",
+    component: Home
+  },
+
+  {
+    path: "cadastroAtleta",
+    component: AtletaComponent
+  }
+];
