359c67d1d3705e903dbf3e378b10db11fc187b18
Joao <joao.a.guedes@aluno.senai.br>
2026-08-13 20:52:18 -0300

    criado o arquivo service

---
+++ src/app/service/atleta-service.ts
@@ -0,0 +1,12 @@
+import { Pessoa } from '../models/pessoa';
+import { Injectable } from '../../../node_modules/@angular/core/types/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AtletaService {
+  private atleta: Pessoa[] =[]
+
+  
+
+}
