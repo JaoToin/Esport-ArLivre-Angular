992f82e557b67c4c25301e6d1c793b34329a5d98
Joao <joao.a.guedes@aluno.senai.br>
2026-08-13 20:42:02 -0300

    modificando o css e html do atleta-component e ligando ele ao ts, iniciando as configurações no models e criando a pasta models

--- src/app/component/menu-component/atleta-component/atleta-component.ts
+++ src/app/component/menu-component/atleta-component/atleta-component.ts
@@ -1,9 +1,38 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-atleta-component',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './atleta-component.html',
   styleUrl: './atleta-component.css',
 })
-export class AtletaComponent {}
+export class AtletaComponent {
+
+  //DECLARAÇÃO DOS ATRIBUTOS DO COMPONENT
+  nome = ''
+  cpf = ''
+  sexo = ''
+  cep = ''
+  ruaLogradouro = ''
+  bairro = ''
+  cidade = ''
+  uf = ''
+
+  //DECLARAÇÃO DE FUNÇÕES
+  exibeDados(){
+    console.log(this.nome, this.cpf, this.sexo, this.cep, this.ruaLogradouro, this.bairro, this.cidade, this.uf)
+  }
+
+
+  limpar(){
+    this.nome
+    this.cpf
+    this.cep
+    this.bairro
+    this.sexo
+    this.ruaLogradouro
+    this.cidade
+    this.uf
+  }
+}
