7b2cd43dd764b6bcc69d2712fcb404ee5e3ffa1e
Joao <joao.a.guedes@aluno.senai.br>
2026-08-14 18:42:28 -0300

    ajustando o css para ficar responsivel, e organizando as pastas para fora da pasta menu.

---
+++ src/app/component/atleta-component/atleta-component.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AtletaService } from '../../service/atleta-service';
+import { Pessoa } from '../../models/pessoa';
+
+@Component({
+  selector: 'app-atleta-component',
+  imports: [FormsModule],
+  templateUrl: './atleta-component.html',
+  styleUrl: './atleta-component.css',
+})
+export class AtletaComponent {
+
+  //DECLARAÇÃO DOS ATRIBUTOS DO COMPONENT
+  nome = ''
+  cpf = 0
+  sexo = ''
+  cep = 0
+  ruaLogradouro = ''
+  bairro = ''
+  cidade = ''
+  uf = ''
+
+  //DECLARAÇÃO DO CONSTRUTOR
+  constructor(private atletaService: AtletaService ){}
+
+  //DECLARAÇÃO DE FUNÇÕES
+  exibeDados(){
+    console.log(this.nome, this.cpf, this.sexo, this.cep, this.ruaLogradouro, this.bairro, this.cidade, this.uf)
+  }
+
+  salvarAtleta(){
+    const pessoaAtleta = new Pessoa()
+    pessoaAtleta.nome = this.nome
+    pessoaAtleta.cpf = this.cpf
+    pessoaAtleta.sexo = this.sexo
+    pessoaAtleta.cep = this.cep
+    pessoaAtleta.rua = this.ruaLogradouro
+    pessoaAtleta.bairro = this.bairro
+    pessoaAtleta.cidade = this.cidade
+    pessoaAtleta.uf = this.uf
+
+    this.atletaService.adicionar(pessoaAtleta)
+
+    this.atletaService.listar()
+    this.limpar()
+  }
+
+
+  limpar(){
+    this.nome = ''
+    this.cpf = 0
+    this.cep = 0
+    this.bairro = ''
+    this.sexo = ''
+    this.ruaLogradouro = ''
+    this.cidade = ''
+    this.uf = ''
+  }
+}
